fe923b9374496a9f19ad70e92c54662d042c49c4
koyoujin6759 <rhdbwls853@naver.com>
2025-01-11 22:47:06 +0900

    build 오류 수정

--- app/(movies)/movies/[id]/page.jsx
+++ app/(movies)/movies/[id]/page.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import MovieInfo from "@/components/movie-info";
 import MovieVideos from "@/components/movie-videos";
 import styles from "../../../../styles/movie.module.css";
-import { getMovie } from "@/components/movie-info";
+import MovieInfo, { getMovie } from "@/components/movie-info";
 
 export async function generateMetadata({ params: { id } }) {
   const movie = await getMovie(id);
