768ba1bfb6602757eb3f8d422dd1e35be9848c1a
koyoujin6759 <rhdbwls853@naver.com>
2025-01-11 19:55:41 +0900

    250111

--- app/(movies)/movies/[id]/page.jsx
+++ app/(movies)/movies/[id]/page.jsx
@@ -1,3 +1,18 @@
-export default function MovieDetail({ params: { id } }) {
-  return <div>Movie Page {id}</div>;
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id) {
+  const response = await fetch(`${API_URL}/${id}`);
+  const movie = await response.json();
+  return movie;
+}
+
+export default async function MovieDetail({ params: { id } }) {
+  const movie = await getMovie(id);
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <p>{movie.rating}</p>
+      <p>{movie.description}</p>
+    </div>
+  );
 }
